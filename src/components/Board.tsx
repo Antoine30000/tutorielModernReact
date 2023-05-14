bc9825d9f7a0952ce196af405383b4980eca3b71
antoine30000 <antoine.beaurin@eemi.com>
2023-05-14 09:06:04 -0700

    feat: winner or nah

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -23,6 +23,36 @@ const renderSquare = (index: number) => {
     );
   }
 
+  const checkWin=(squares:Array<string | null>): string | null=> {
+    const winningLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winningLines.length; i++) {
+      const [a, b, c] = winningLines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = checkWin(squares);
+  let status;
+  if (winner){
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${currentPlayer}`;
+  }
+  
+
   return (
     <div>
       <div className="board-row">
@@ -40,6 +70,7 @@ const renderSquare = (index: number) => {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
+      {status}
     </div>
   );
 }
